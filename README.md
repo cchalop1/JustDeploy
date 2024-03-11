889f9f0be2ef58ba919fe58653d7482db88acf6a
cchalopin <clement.chalopin@gmail.com>
2024-03-11 11:51:12 +0100

    feat: add database adapter
    update: Readme

--- README.md
+++ README.md
@@ -80,7 +80,7 @@ Pull requests are welcome. For major changes, please open an issue first to disc
 
 ## License
 
-This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+This project is licensed under the AGPLv3 License - see the [LICENSE](LICENSE) file for details.
 
 ## Contact
 
--- internal/adapter/database.go
+++ internal/adapter/database.go
@@ -1,5 +1,7 @@
 package adapter
 
+import "cchalop1.com/deploy/internal/domain"
+
 type DatabaseAdapter struct {
 }
 
@@ -7,10 +9,11 @@ func NewDatabaseAdapter() *DatabaseAdapter {
 	return &DatabaseAdapter{}
 }
 
-func (d *DatabaseAdapter) GetState() {
+func (d *DatabaseAdapter) GetState() domain.DeployConfigDto {
 
+	return domain.DeployConfigDto{}
 }
 
-func (d *DatabaseAdapter) SaveState() {
-
+func (d *DatabaseAdapter) SaveState(deployConfig domain.DeployConfigDto) error {
+	return nil
 }
