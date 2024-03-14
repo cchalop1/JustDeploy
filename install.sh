9f874e6182df3d2301dba8a9e276edce7d690440
cchalopin <clement.chalopin@gmail.com>
2024-03-14 11:56:35 +0100

    fix: install script

--- install.sh
+++ install.sh
@@ -2,19 +2,25 @@
 
 # Define the release URL and the binary file name
 release_url="https://api.github.com/repos/cchalop1/JustDeploy/releases/latest"
-binary_file="justdeploy.zip"
+zip_file="justdeploy.zip"
+binary_file="justdeploy"
 
 # Get the latest release download URL for the specific platform
 response=$(curl -s $release_url)
-download_url=$(echo $response | grep -o "https://github.com/your_username/your_repo/releases/download/[^ ]*/$binary_file" | head -n 1)
+download_url=$(echo $response | grep -o "https://github.com/cchalop1/JustDeploy/releases/download/[^ ]*/$zip_file" | head -n 1)
 
 # Download the binary
-curl -L -o $binary_file $download_url
+curl -L -o $zip_file $download_url
+
+# Unzip binary file
+unzip $zip_file
 
 # Make the binary executable
-chmod +x $binary_file
+chmod +x bin/$binary_file
 
 # Move the binary to a system directory (e.g., /usr/local/bin)
-sudo mv $binary_file /usr/local/bin/
+sudo mv bin/$binary_file /usr/local/bin/
 
-echo "Installation complete. You can now run $binary_file"
+rm $zip_file
+
+echo "✨ Installation complete. You can now run $binary_file"
