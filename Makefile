9cb9a43a75d8c55fb832906a10136a806a28e459
cchalopin <clement.chalopin@gmail.com>
2024-03-14 16:36:22 +0100

    feat: add build for platformes

--- Makefile
+++ Makefile
@@ -1,20 +1,35 @@
-GOBIN = ./bin
+BINDIR 			= ./bin
+EXECUTABLE  	= justdeploy
 
 all: build-web copy-web-build build-go
 
-copy-web-build:
-	cp -R web/dist internal/web/dist/
+build-ci: build-web copy-web-build build-darwin-arm build-darwin-x86 build-linux-arm build-linux-x86
+
+build-darwin-arm:
+	@env GOOS=darwin GOARCH=arm64 go build -o $(BINDIR)/$(EXECUTABLE)-darwin-arm ./cmd/just-deploy/main.go
+
+build-darwin-x86:
+	@env GOOS=darwin GOARCH=amd64 go build -o $(BINDIR)/$(EXECUTABLE)-darwin-x86 ./cmd/just-deploy/main.go
+
+build-linux-arm:
+	@env GOOS=linux GOARCH=arm64 go build -o $(BINDIR)/$(EXECUTABLE)-linux-arm ./cmd/just-deploy/main.go
+
+build-linux-x86:
+	@env GOOS=linux GOARCH=amd64 go build -o $(BINDIR)/$(EXECUTABLE)-linux-x86 ./cmd/just-deploy/main.go
 	
 build-go:
-	@go build -o $(GOBIN)/justdeploy ./cmd/just-deploy/main.go
+	@go build -o $(BINDIR)/$(EXECUTABLE) ./cmd/just-deploy/main.go
 
 build-web:
 	@cd web && pnpm install && pnpm run build
 
+copy-web-build:
+	cp -R web/dist internal/web/dist/
+
 run:
-	@$(GOBIN)/justdeploy
+	@$(BINDIR)/$(EXECUTABLE)
 
 clean:
-	@rm -f $(GOBIN)/justdeploy
+	@rm -rf $(BINDIR)
 	@cd web && rm -rf dist
 	@rm -r internal/web/dist
--- install.sh
+++ install.sh
@@ -5,6 +5,28 @@ release_url="https://api.github.com/repos/cchalop1/JustDeploy/releases/latest"
 zip_file="justdeploy.zip"
 binary_file="justdeploy"
 
+# Get the current platform and architecture
+platform=$(uname -s | tr '[:upper:]' '[:lower:]')
+arch=$(uname -m)
+
+if [ "$platform" == "darwin" ]; then
+  if [ "$arch" == "arm64" ]; then
+    zip_file="justdeploy-darwin-arm.zip"
+    binary_file="justdeploy-darwin-arm"
+  else
+    zip_file="justdeploy-darwin-x86.zip"
+    binary_file="justdeploy-darwin-x86"
+  fi
+else
+  if [ "$(expr substr $(uname -m) 1 5)" == "armv7" ] || [ "$(expr substr $(uname -m) 1 3)" == "aarch64" ]; then
+    zip_file="justdeploy-linux-arm.zip"
+    binary_file="justdeploy-linux-arm"
+  else
+    zip_file="justdeploy-linux-x86.zip"
+    binary_file="justdeploy-linux-x86"
+  fi
+fi
+
 # Get the latest release download URL for the specific platform
 response=$(curl -s $release_url)
 download_url=$(echo $response | grep -o "https://github.com/cchalop1/JustDeploy/releases/download/[^ ]*/$zip_file" | head -n 1)
@@ -13,13 +35,13 @@ download_url=$(echo $response | grep -o "https://github.com/cchalop1/JustDeploy/
 curl -L -o $zip_file $download_url
 
 # Unzip binary file
-unzip $zip_file
+unzip $binary_file $zip_file
 
 # Make the binary executable
-chmod +x bin/$binary_file
+chmod +x $binary_file
 
 # Move the binary to a system directory (e.g., /usr/local/bin)
-sudo mv bin/$binary_file /usr/local/bin/
+sudo mv $binary_file /usr/local/bin/
 
 rm $zip_file
 
