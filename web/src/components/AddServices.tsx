d4e1fef03d80c913cf58349fae1bbbd4335f826d
cchalopin <clement.chalopin@gmail.com>
2024-04-12 14:34:47 +0200

    fix: typo

--- web/src/components/AddServices.tsx
+++ web/src/components/AddServices.tsx
@@ -61,8 +61,8 @@ export default function AddService({
         className="w-full mb-3"
         onClick={() => setOpen(true)}
       >
-        Click here for create and connect new database or pres{" "}
-        <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+        Click here for create and connect new database or press{" "}
+        <kbd className="ml-2 pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs">⌘</span>K
         </kbd>
       </Button>
