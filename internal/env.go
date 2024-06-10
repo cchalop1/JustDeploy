06dfd3db3b667dba526af1defc69930a937a87c1
cchalopin <clement.chalopin@gmail.com>
2024-06-10 11:53:33 +0200

    fix: don't hide the config folder

--- internal/env.go
+++ internal/env.go
@@ -1,6 +1,6 @@
 package internal
 
-const JUSTDEPLOY_FOLDER = "/Users/cchalop1/.config/" + ".justdeploy"
+const JUSTDEPLOY_FOLDER = "/Users/cchalop1/.config/" + "justdeploy"
 
 const CERT_DOCKER_FOLDER = JUSTDEPLOY_FOLDER + "/cert-docker"
 
