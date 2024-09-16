cd8f5db9e714fead293724f0bbc2331f0e51f3f8
cchalopin <clement.chalopin@gmail.com>
2024-09-16 14:59:25 +0200

    feat: add fonts

--- web/src/components/project/ProjectPageHeader.tsx
+++ web/src/components/project/ProjectPageHeader.tsx
@@ -1,15 +1,8 @@
-import { Button } from "../ui/button";
-
 export default function ProjectPageHeader() {
   return (
-    <div className="flex justify-between">
-      <div className="font-bold text-3xl">JustDeploy 🛵</div>
-      <div className="p-2 flex flex-row-reverse gap-3 items-center bg-white w-1/4 rounded-lg border shadow-lg">
-        <Button onClick={() => {}}>Deploy</Button>
-        {/* <Button variant="link" onClick={() => setOpen(true)}>
-          Create +
-        </Button> */}
-      </div>
+    <div className="flex justify-between pl-10 pr-10 pt-6 pb-6">
+      <div className="">JustDeploy</div>
+      <div className="p-2 flex flex-row-reverse gap-3 items-center bg-white w-1/4 rounded-lg border shadow-lg"></div>
     </div>
   );
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -71,6 +71,10 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        'sans': ['Geist', 'sans-serif'],
+        'mono': ['GeistMono', 'monospace'],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
