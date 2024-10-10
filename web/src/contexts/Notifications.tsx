7d78b5478746cd37163f1724bce1f2e9ea53c869
cchalopin <clement.chalopin@gmail.com>
2024-10-10 15:58:09 +0200

    fix: toaster to shacn toasters

--- web/src/contexts/Notifications.tsx
+++ web/src/contexts/Notifications.tsx
@@ -1,7 +1,6 @@
-import DisplayNotifications from "@/components/alerts/DisplayNotifications";
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/hooks/use-toast";
 import React, { createContext, useState, ReactNode } from "react";
-
-const NOTIF_DISPLAY_TIME = 7000;
 
 type NotificationType = "success" | "error" | "info" | "warning";
 
@@ -26,7 +25,6 @@ export interface NotificationContextProps {
   error: NotificationFunc;
   info: NotificationFunc;
   warning: NotificationFunc;
-  removeNotification: (id: string) => void;
 }
 
 export const NotificationContext = createContext<
@@ -36,51 +34,46 @@ export const NotificationContext = createContext<
 export const NotificationProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const [notifications, setNotifications] = useState<Notification[]>([]);
+  const { toast } = useToast();
 
   const addNotification = (
     type: NotificationType,
     title: string,
-    content: string,
-    onConfirm?: () => void
+    content: string
   ) => {
-    const id = Math.random().toString(36).substr(2, 9);
-    setNotifications([
-      ...notifications,
-      { id, type, title, content, onConfirm },
-    ]);
-    setTimeout(() => {
-      setNotifications((prevNotifications) => prevNotifications.slice(1));
-    }, NOTIF_DISPLAY_TIME);
+    let variant = "default";
+    if (type === "error") {
+      variant = "destructive";
+    }
+    if (type === "warning") {
+      title = `⚠️ ${title}`;
+    }
+    if (type === "success") {
+      title = `✅ ${title}`;
+    }
+    toast({
+      title,
+      description: content,
+      variant: variant as any,
+    });
   };
 
-  const removeNotification = (id: string) => {
-    setNotifications(
-      notifications.filter((notification) => notification.id !== id)
-    );
-  };
+  const success: NotificationFunc = ({ content, title }) =>
+    addNotification("success", title, content);
 
-  const success: NotificationFunc = ({ content, title, onConfirm }) =>
-    addNotification("success", title, content, onConfirm);
+  const error: NotificationFunc = ({ content, title }) =>
+    addNotification("error", title, content);
 
-  const error: NotificationFunc = ({ content, title, onConfirm }) =>
-    addNotification("error", title, content, onConfirm);
+  const info: NotificationFunc = ({ content, title }) =>
+    addNotification("info", title, content);
 
-  const info: NotificationFunc = ({ content, title, onConfirm }) =>
-    addNotification("info", title, content, onConfirm);
-
-  const warning: NotificationFunc = ({ content, title, onConfirm }) =>
-    addNotification("warning", title, content, onConfirm);
+  const warning: NotificationFunc = ({ content, title }) =>
+    addNotification("warning", title, content);
 
   return (
-    <NotificationContext.Provider
-      value={{ success, error, info, warning, removeNotification }}
-    >
-      <DisplayNotifications
-        notifications={notifications}
-        removeNotif={(notif) => removeNotification(notif.id)}
-      />
+    <NotificationContext.Provider value={{ success, error, info, warning }}>
       {children}
+      <Toaster />
     </NotificationContext.Provider>
   );
 };
