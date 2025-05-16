d29be967e07c9e98e61e151c20ed4f1167d7054d
cchalopin <clement.chalopin@gmail.com>
2025-05-16 10:30:35 +0800

    chore: add debug log to ManageGithubEvent function for improved event handling

--- internal/application/manage-github-event.go
+++ internal/application/manage-github-event.go
@@ -1,12 +1,15 @@
 package application
 
 import (
+	"fmt"
+
 	"cchalop1.com/deploy/internal/api/dto"
 	"cchalop1.com/deploy/internal/api/service"
 )
 
 func ManageGithubEvent(deployService *service.DeployService, githubEvent dto.GithubEvent) error {
 	var err error
+	fmt.Println(githubEvent)
 
 	if githubEvent.Ref == "refs/heads/main" {
 		err = ReDeployApplication(deployService, githubEvent.Repository.Name)
